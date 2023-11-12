d5f7442079345a2c1e60b7608fa8975dff773ecf
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-11-12 23:49:44 +0500

    Task7

---
+++ Tyuiu.AbdullinAI.Sprint4.Task7.V23.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.InteropServices;
+using Tyuiu.AbdullinAI.Sprint4.Task7.V23.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint4.Task7.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Validcalculate()
+        {
+            DataService ds = new DataService();
+
+            int n = 4;
+            int m = 3;
+
+            string str = "678135972584";
+            int[,] mtrx = new int[n, m];
+
+            int res = ds.Calculate(n, m, str);
+
+            int wait = 5;
+
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.AbdullinAI.Sprint4.Task7.V23/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AbdullinAI.Sprint4.Task7.V23.Lib;
+
+namespace Tyuiu.AbdullinAI.Sprint4.Task7.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Абудллин А. И.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:                                                                   *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Абдуллин А. И. | АСОиУБ-23-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр \"678135972584\".                     *");
+            Console.WriteLine("* Преобразуйте ее в матрицу 4 на 3 и подсчитайте количество четных чисел. *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            int rows = 4, columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            string str = "678135972584";
+            int index = 0;
+
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество чётных элементов: " + ds.Calculate(rows, columns, str));
+            Console.ReadKey();
+        }
+    }
+}
